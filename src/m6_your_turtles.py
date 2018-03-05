325251d14362c397a7329c6d0639ba981a8bd3ba
novotnaj <novotnaj@rose-hulman.edu>
2018-03-05 11:30:55 -0500

    Completed all

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Andrew Novotny.
 """
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
@@ -27,3 +27,29 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+red = rg.SimpleTurtle()
+red.pen = rg.Pen('red',2)
+red.speed = 10
+
+grey = rg.SimpleTurtle()
+grey.pen = rg.Pen('grey',2)
+grey.speed = 20
+
+size_red = 100
+size_grey = 10
+
+for k in range(101):
+    red.forward(size_red)
+    grey.backward(size_grey)
+    red.right(90)
+    grey.right(90)
+
+    size_red = size_red + 10
+    size_grey = size_grey - 10
+
+window.close_on_mouse_click()
